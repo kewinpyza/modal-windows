7fbb8f201e16b0d73c84bde44d86858147f2e969
kewinpyza <kewin.pyza@gmail.com>
2022-12-12 01:53:04 +0100

    Add functionalities and js

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const showModalButtons = document.querySelector('.btn-show');
 const showWarning = document.querySelector('.modal-warning');
 const showError = document.querySelector('.modal-error');
 const showDefault = document.querySelector('.modal-default');
+const overlay = document.querySelector('.overlay');
 
 const modal = (obj = {}) => {
   return `
-    <div class="modal-window">
+    <div tabindex="5" class="modal-window">
       <button class="btn closer">&times;</button>
       <p class="modal-title ${obj.type || 'info'}">
         <i class="icon fa-solid fa-${obj.icon || 'circle-info'}"></i>
@@ -20,8 +21,10 @@ const modal = (obj = {}) => {
         }
       </p>
       <div class="modal-buttons">
-        <button class="modal-accept">${obj.acceptBtn || 'Accept'}</button>
-        <button class="modal-reject">${obj.rejectBtn || 'Reject'}</button>
+        <button class="modal-btn modal-accept">${
+          obj.acceptBtn || 'Yes'
+        }</button>
+        <button class="modal-btn modal-reject">${obj.rejectBtn || 'No'}</button>
       </div>
     </div>
     `;
@@ -32,34 +35,70 @@ const createModal = objValues => {
   modalContainer.classList.add('modal-container');
   modalContainer.innerHTML = modal(objValues);
   document.body.append(modalContainer);
+  const selectModalContainer = document.querySelector('.modal-container');
+  const closeModal = () => {
+    selectModalContainer.remove();
+    document.body.style.overflow = 'auto';
+    overlay.classList.add('hidden');
+  };
+
+  const closeIcon = document.querySelector('.closer');
+  closeIcon.addEventListener('click', () => {
+    closeModal();
+  });
+
+  const modalButtons = document.querySelectorAll('.modal-btn');
+  modalButtons.forEach(btn =>
+    btn.addEventListener('click', () => {
+      closeModal();
+    })
+  );
+
+  const selectModalWindow = document.querySelector('.modal-window');
+  selectModalWindow.focus();
+  selectModalWindow.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      console.log('Elo');
+      closeModal();
+    }
+  });
+
+  overlay.addEventListener('click', () => {
+    closeModal();
+  });
+
+  document.body.style.overflow = 'hidden';
 };
 
 const displayWarningModal = () => {
   createModal({
     type: 'warning',
     icon: 'triangle-exclamation',
-    title: 'Lorem ipsum dolor sit amet',
+    title: 'Warning! Are you sure?',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inciduntquidem totam corporis mollitia minima? Soluta facere labore vitae tempora recusandae dolorum ullam explicabo officia consequuntur suscipit dignissimos, vero voluptatibus magnam?',
-    acceptBtn: 'Akceptuj',
-    rejectBtn: 'Anuluj',
+      'Unsaved changes may be lost. Click "Accept" to confirm or "Close" if you do not want to do any changes.',
+    acceptBtn: 'Accept',
+    rejectBtn: 'Close',
   });
+  overlay.classList.remove('hidden');
 };
 
 const displayErrorModal = () => {
   createModal({
     type: 'danger',
     icon: 'circle-exclamation',
-    title: 'Lorem ipsum dolor sit amet',
+    title: "Error! You can't do this!",
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inciduntquidem totam corporis mollitia minima? Soluta facere labore vitae tempora recusandae dolorum ullam explicabo officia consequuntur suscipit dignissimos, vero voluptatibus magnam?',
-    acceptBtn: 'Accept',
+      'Sorry. Your command cannot be fulfilled. For more info, you can contact our Helpdesk if the problem persists.',
+    acceptBtn: 'Ok',
     rejectBtn: 'Reject',
   });
+  overlay.classList.remove('hidden');
 };
 
 const displayDefaultModal = () => {
   createModal();
+  overlay.classList.remove('hidden');
 };
 
 showWarning.addEventListener('click', displayWarningModal);
